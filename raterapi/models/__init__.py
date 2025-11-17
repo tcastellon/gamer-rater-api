0c4466f4953a7486e8e7f32c90b69be249ac668a
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-17 10:06:05 -0600

    added imports

---
+++ raterapi/models/__init__.py
@@ -0,0 +1,6 @@
+from .game import Game
+from .category import Category
+from .game_category import GameCategory
+from .game_image import GameImage
+from .game_rating import GameRating
+from .game_review import GameReview
