5dc3404200ca04a69ffd51bc54b8c93da7debeda
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-17 10:09:04 -0600

    added if statement for media files

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -7,4 +9,8 @@ router = routers.DefaultRouter(trailing_slash=False)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
