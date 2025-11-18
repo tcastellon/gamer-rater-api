6f4a31c703170cb6acb23892e91b08ba47b6ab49
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-17 21:52:59 -0600

    added imports for serializers

--- raterapi/serializers/__init__.py
+++ raterapi/serializers/__init__.py
@@ -1,2 +1,6 @@
 # Serializers will be imported here later
 from .category_serializer import CategorySerializer
+from .game_image_serializer import GameImageSerializer
+from .game_rating_serializer import GameRatingSerializer
+from .game_review_serializer import GameReviewSerializer
+from .game_serializer import GameSerializer
