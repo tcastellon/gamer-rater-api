b4ef06f027a1f5cb2b1f795fca03b84e45722ce3
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-19 10:30:28 -0600

    created the endpoints

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -3,11 +3,20 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+from raterapi.views import CategoryViewSet, GameImageViewSet, GameRatingViewSet, GameReviewViewSet, GameViewSet, login_user, register_user
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'game-images', GameImageViewSet, basename='game-image')
+router.register(r'game-ratings', GameRatingViewSet, basename='game-rating')
+router.register(r'game-reviews', GameReviewViewSet, basename='game-review')
+router.register(r'games', GameViewSet, basename='game')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login', login_user),
+    path('register', register_user),
+    path('admin/', admin.site.urls),
 ]
 
 # Serve media files during development
