631e0d92f5f403dfc4c9d22c8b55755064adb305
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-11-19 10:29:37 -0600

    added imports for ViewSets

---
+++ raterapi/views/__init__.py
@@ -0,0 +1,6 @@
+from .category_view import CategoryViewSet
+from .game_view import GameViewSet
+from .game_rating_view import GameRatingViewSet
+from .game_review_view import GameReviewViewSet
+from .game_image_view import GameImageViewSet
+from .auth import register_user, login_user
